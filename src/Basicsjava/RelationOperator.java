2494ff98c0c4f43704ea9e544a3728e2d677387d
Rohit <rohit@example.com>
2026-08-09 22:32:38 +0530

    first commit

---
+++ src/Basicsjava/RelationOperator.java
@@ -0,0 +1,18 @@
+package Basicsjava;
+
+public class RelationOperator {
+    public static void main(String[] args) {
+        int a =22;
+        int b = 10;
+        int c = 30;
+
+        System.out.println(a>b);
+        System.out.println(c<b);
+        System.out.println(a>=c);
+        System.out.println(a<=b);
+        System.out.println(c==b);
+        System.out.println(a!=c);
+
+
+    }
+}
